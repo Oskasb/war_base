63ca5ce126ffff06cb2b7ffc1656ca0ee43b2bd4
oskar <oskar.asbrink@gmail.com>
2017-08-16 15:49:10 +0200

    add config for phonegap

--- client/js/lib/data_pipeline/src/worker/XhrThing.js
+++ client/js/lib/data_pipeline/src/worker/XhrThing.js
@@ -1,4 +1,4 @@
-"use strict";
+
 
 define([
 
@@ -9,11 +9,117 @@ define([
 
         var fileServerUrl = "http://localhost:5001";
 
+        var paths = [];
+        var fileSystem;
+
+        var setupFileReader = function() {
+            function onLoad() {
+                document.addEventListener("deviceready", onDeviceReady, false);
+            }
+
+
+
+            function gotFS(fileSystem) {
+                fileSystem.root.getFile("readme.txt", null, gotFileEntry, fail);
+            }
+
+            function gotFileEntry(fileEntry) {
+                fileEntry.file(gotFile, fail);
+            }
+
+            function gotFile(file){
+                readDataUrl(file);
+                readAsText(file);
+            }
+
+            function readDataUrl(file) {
+                var reader = new FileReader();
+                reader.onloadend = function(evt) {
+                    console.log("Read as data URL");
+                    console.log(evt.target.result);
+                };
+                reader.readAsDataURL(file);
+            }
+
+            function readAsText(file) {
+                var reader = new FileReader();
+                reader.onloadend = function(evt) {
+                    console.log("Read as text");
+                    console.log(evt.target.result);
+                };
+                reader.readAsText(file);
+            }
+
+            function fail(evt) {
+                console.log(evt.target.error.code);
+            }
+
+
+
+            function getAllEntries(dirReader) {
+                var entries = dirReader.readEntries();
+
+                for (var i = 0, entry; entry = entries[i]; ++i) {
+                    paths.push(entry.toURL()); // Stash this entry's filesystem: URL.
+
+                    // If this is a directory, we have more traversing to do.
+                    if (entry.isDirectory) {
+                        getAllEntries(entry.createReader());
+                    }
+                }
+            }
+
+            // PhoneGap is ready
+            //
+        //    function onDeviceReady() {
+            fileSystem = self.requestFileSystemSync(PERSISTENT, 0 ,  gotFS, fail);
+            console.log("File Sys:", fileSystem);
+            getAllEntries(fileSystem.root.createReader());
+
+            var fileEntry = fileSystem.root.getFile('testFile.blob', {create: true});
+
+            //    postMessage('Got file entry.');
+
+            //    var arrayBuffer = makeRequest(request.url);
+            var blob = new Blob([0], {type: 'arraybuffer'});
+                            //        postMessage('Begin writing');
+                fileEntry.createWriter().write(blob);
+
+            console.log("Paths:", paths, blob);
+        //    gotFS(fileSystem);
+
+        //    }
+
+        };
+
 		var XhrThing = function() {
 
 		};
 
 		XhrThing.prototype.sendXHR = function(packet, successCallback, failCallback) {
+
+			if (paths.indexOf(packet.url) !== -1) {
+			//	console.log("File Reader is present!", LocalFileSystem);
+
+                    var pkt = packet;
+                    var reader = new FileReaderSync();
+                    reader.onloadend = function(evt) {
+                        console.log("read success");
+                        console.log(evt.target.result);
+                        successCallback(evt.target.result, pkt);
+                    };
+                    reader.onerror = function(err) {
+                        failCallback(pkt.url, "File Reader Error", err);
+                    };
+
+                    if (pkt.responseType === 'arraybuffer') {
+                        reader.readAsDataURL(pkt.url);
+                    } else {
+                        reader.readAsText(pkt.url);
+                    }
+                    return;
+
+			}
 
 			var body = "";
 
@@ -49,7 +155,7 @@ define([
 
 			request.onreadystatechange = function() {
 				if (request.readyState == 4) {
-					successCallback(request.response, request.packet);
+				    successCallback(request.response, request.packet);
 				}
 			};
 
