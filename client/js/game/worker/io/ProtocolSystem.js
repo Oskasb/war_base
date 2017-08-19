d9a26c0dc16a50ff1e1cb29469eb93ced88aa732
oskar <oskar.asbrink@gmail.com>
2017-08-19 02:33:10 +0200

    clean it

--- client/js/game/worker/io/ProtocolSystem.js
+++ client/js/game/worker/io/ProtocolSystem.js
@@ -1,14 +1,6 @@
 "use strict";
 
-define(['worker/physics/CannonAPI',
-        'worker/terrain/TerrainFunctions',
-        'worker/io/ProtocolSystem'
-    ],
-    function(
-        CannonAPI,
-        TerrainFunctions,
-        ProtocolSystem
-    ) {
+define([], function() {
 
         var ProtocolSystem = function() {
             this.protocols = {};
@@ -40,33 +32,8 @@ define(['worker/physics/CannonAPI',
             }
         };
 
-
         ProtocolSystem.prototype.addProtocol = function(protocol) {
-
             this.protocols[protocol[0]] = protocol;
-
-            var randomSend = function(prtc) {
-
-                var channelCount = (prtc.length - 3) / 3;
-                var channel = 2 + 3 * Math.floor(Math.random()*channelCount);
-
-
-                prtc[channel+1] = Math.random();
-
-                prtc[prtc.length-1] = 1000*Math.random()*0.01;
-
-                if (self.SharedArrayBuffer) {
-                    prtc[channel+2][0] = prtc[channel+1];
-                    prtc[channel+2][1] = 1000 * Math.random() * 0.01;
-                } else {
-                    this.postToMain([prtc[0], prtc[1], channel, prtc[channel+1], prtc[prtc.length-1]]);
-                }
-
-            }.bind(this);
-
-            setInterval(function() {
-                //        randomSend(protocol);
-            },40);
         };
 
         ProtocolSystem.prototype.handleProtocolMessage = function(protocol) {
@@ -80,7 +47,7 @@ define(['worker/physics/CannonAPI',
 
             var target = this.protocols[protocol[1]];
 
-            var msg = [target[0], target[1]]
+            var msg = [target[0], target[1]];
 
 
             var addStateToMessage = function(channelKey, targetKey) {
@@ -90,13 +57,12 @@ define(['worker/physics/CannonAPI',
 
                 target[targetChannel+1] = protocol[sourceChannel+1];
 
-
                 if (self.SharedArrayBuffer) {
                     target[targetChannel+2][0] = target[targetChannel+1];
-                    target[targetChannel+2][1] = 1000 * Math.random() * 0.01;
+                    target[targetChannel+2][1] = 0.2;
                 } else {
-                    msg.push(targetChannel+1);
-                    msg.push(target[targetChannel+2]);
+                    msg.push(targetChannel);
+                    msg.push(target[targetChannel+1]);
                 }
 
             };
@@ -106,21 +72,13 @@ define(['worker/physics/CannonAPI',
                 i++
             }
 
-            msg.push(1000*Math.random()*0.01);
+            if (self.SharedArrayBuffer) return;
 
-            this.postToMain(msg);
-
+            msg.push(0.2);
+            self.postMessage(msg);
         };
 
 
-        ProtocolSystem.prototype.saveJsonData = function(json, url) {
-            this.worker.postMessage(['storeJson', url, json]);
-        };
-
-
-        ProtocolSystem.prototype.postToMain = function(msg) {
-            postMessage(msg);
-        };
 
         return ProtocolSystem;
 
